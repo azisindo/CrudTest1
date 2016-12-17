e731fa929cfd61bf3ac30c7b06c064599e6ff52c
azisindo <aziz.mcgr@gmail.com>
2016-12-17 09:33:54 +0700

    Add project files.

---
+++ CrudTest1/DbConnection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace CrudTest1
+{
+    public class DbConnection
+    {
+        private MySqlConnection conn = null;
+        private static DbConnection dbconn = null;
+        //constructor
+        private DbConnection()
+        {
+            if (conn==null)
+            {
+                //membaca data xml
+                string server   = ConfigurationSettings.AppSettings["Server"];
+                string database = ConfigurationSettings.AppSettings["Database"];
+
+                string user      = "root";
+                string password  = "password";
+                string strConn   = "SERVER=" + server + ";DATABASE=" + database + ";UID=" + user + ";PASSWORD=" + password;
+                conn = new MySqlConnection(strConn);
+                conn.Open();
+            }
+        }
+
+        public static DbConnection GetInstance()
+        {
+            if (dbconn==null)
+            {
+                dbconn = new DbConnection();
+            }
+            return dbconn;
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            return this.conn;
+
+        }
+    }
+}
---
+++ CrudTest1/Model/Karyawan.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudTest1.Model
+{
+    public class Karyawan
+    {
+        private string _nik;
+        public string nik
+        {
+            get { return _nik; }
+            set { _nik = value; }
+        }
+
+        private string _nama;
+        public string nama
+        {
+            get { return _nama; }
+            set { _nama = value; }
+        }
+                
+        private DateTime _tglLahir;
+        public DateTime tglLahir
+        {
+            get { return _tglLahir; }
+            set { _tglLahir = value; }
+        }
+
+        private String _alamat;
+        public string alamat
+        {
+            get { return _alamat; }
+            set { _alamat = value; }
+        }
+
+        private Int32 _jmlLembur;
+        public Int32 jmllembur
+        {
+            get { return _jmlLembur; }
+            set { _jmlLembur = value; }
+        }
+
+        private double _pajak;
+        public double pajak
+        {
+            get { return _pajak; }
+            set { _pajak = value; }
+        }
+
+        private int _total;
+        public int total
+        {
+            get { return _total; }
+            set { _total = value; }
+        }
+            
+    }
+
+
+}
